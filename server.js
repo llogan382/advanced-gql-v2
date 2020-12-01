caa2df364dc76bba5d37620ed1be42662667ef44
Luke <llogan382@gmail.com>
2020-12-01 12:52:03 -0500

    adds new server file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+
+const {ApolloServer} = require('apollo-server')
 const gql = require('graphql-tag')
 
 const typeDefs = gql`
     type User {
-        id: ID!
-        username: String!
-        createdAt: Int!
+        id: ID
+        username: String
+        createdAt: Int
     }
 
     type Settings {
@@ -12,8 +14,58 @@ const typeDefs = gql`
         theme: String!
     }
 
-    type Query {
-
+    input NewSettingsInput {
+        user: ID!
+        theme: String!
     }
 
+    type Query {
+        me: User
+        settings(user: ID!): Settings!
+    }
+
+    type Mutation {
+        settings(input: NewSettingsInput!): Settings!
+    }
 `
+
+const resolvers = {
+    Query: {
+        me() {
+            return {
+                id: '2345234',
+                username: 'Coder12',
+                createdAt: 413578123
+            }
+        },
+        settings(_, {user}){
+            return {
+                user,
+                theme: 'Light'
+
+            }
+        }
+    },
+    Mutation: {
+        settings(_, {input}){
+            return input
+        }
+    },
+
+    Settings: {
+        user(settings){
+            return {
+                id: 1,
+                username: 'Coder12',
+                createdAt: 413578123
+            }
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({url}) => console.log(`server running at ${url}`));
